cf89816788cfecd555ad8234e5b5f7decf649994
dave david <dave-poissant>
2020-02-01 17:12:24 -0500

    Added glider for seconds on a sign

--- Ui_Main_Script.py
+++ Ui_Main_Script.py
@@ -3,19 +3,43 @@ from tkinter import messagebox
 
 
 class application(Frame):
-    def createWidgets(self):
-        self.options_label = Label(self, text="Options:")
-        self.options_label.pack()
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.create_widgets()
 
-        self.stop_button = Button(self)
+    def create_hand_control_frame(self):
+        hand_control_frame = Frame(self)
+
+        self.stop_button = Button(hand_control_frame)
         self.stop_button["text"] = "stop"
         self.stop_button["command"] = self.say_stoped
-        self.stop_button.pack()
+        self.stop_button.pack(side=LEFT)
 
-        self.pause_button = Button(self)
+        self.pause_button = Button(hand_control_frame)
         self.pause_button["text"] = "pause",
         self.pause_button["command"] = self.say_paused
-        self.pause_button.pack(side=LEFT, pady=5, padx=5)
+        self.pause_button.pack(side=LEFT)
+
+        hand_control_frame.pack(side=LEFT)
+
+    def create_options_frame(self):
+        options_frame = Frame(self)
+        option_label_frame = Frame(options_frame)
+        option_label_frame.pack(side=TOP)
+
+        self.options_label = Label(option_label_frame, text="Options:")
+        self.options_label.pack(side=LEFT)
+
+        self.next_letter_speed_glider = Scale(options_frame, label="Second(s) passed on one sign", orient=HORIZONTAL,
+                                              to=5.0, from_=1.0, tickinterval=0.25, length=169)
+        self.next_letter_speed_glider.pack(side=TOP, pady=10)
+
+        options_frame.pack(side=TOP)
+
+    def create_widgets(self):
+        self.create_options_frame()
+        self.create_hand_control_frame()
 
     def connect(self, widget, signal, event):
         widget.bind(signal, event)
@@ -28,14 +52,10 @@ class application(Frame):
         # TODO: Add sending message to the arduino here (and wait for the "message received" before showing info)
         messagebox.showinfo("Paused", "Hand has been paused.")
 
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
-
 
 if __name__ == "__main__":
     root = Tk("")
     app = application(master=root)
+    root.geometry('640x640')
     app.mainloop()
     root.destroy()
